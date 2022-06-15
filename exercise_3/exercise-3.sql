3eadc176633be7cad4d5805880e3d2862d2813f2
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-15 13:12:04 -0700

    third test passing

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,10 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+
+SELECT
+film.title
+from film
+LEFT JOIN film_category on film.title = film_category.film_id
+LEFT JOIN category on film_category.category_id = category.category_id
+WHERE category.category_id = 'Action';
